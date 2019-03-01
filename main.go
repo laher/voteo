94a1508f801af6af29b2368221deffc4e4659601
Am Laher <am@movio.co>
2019-03-02 08:08:43 +1300

    db

--- main.go
+++ main.go
@@ -1,18 +1,125 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"sync"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("src"))
 	http.Handle("/", fs)
+	http.HandleFunc("/videos", crudHandler)
 	http.HandleFunc("/yt/data", ytDataHandler)
+	loadVideos()
 	log.Println("Listening...")
 	http.ListenAndServe(":3000", nil)
 }
 
-func ytDataHandler(r *http.Request, w http.ResponseWriter) {
+type video struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Votes int    `json:"votes"`
+}
 
+var (
+	videos     = []video{}
+	initVideos = []video{
+		{ID: "JntjzuI5rGM", Title: "Dave Grohl Tells ...", Votes: 0},
+		{ID: "X7hFERntlog", Title: "Fearless Organization", Votes: 0},
+		{ID: "d_HHnEROy_w", Title: "Stop managing, start ...", Votes: -1},
+		{ID: "BCkCvay4-DQ", Title: "Push Kick", Votes: 1},
+	}
+	lock = sync.RWMutex{}
+)
+
+func loadVideos() {
+	lock.Lock()
+	defer lock.Unlock()
+	b, err := ioutil.ReadFile("videos.json")
+	if err != nil {
+		if os.IsNotExist(err) {
+			videos = initVideos
+			return
+		}
+		log.Fatalf("couldnt read db: %v", err)
+	}
+	myVideos := []video{}
+	err = json.Unmarshal(b, &myVideos)
+	if err != nil {
+		log.Fatalf("Couldnt decode db: %v", err)
+	}
+	videos = myVideos
+}
+
+func writeVideos() {
+	lock.Lock()
+	defer lock.Unlock()
+	b, _ := json.MarshalIndent(videos, "", "  ")
+	err := ioutil.WriteFile("videos.json", b, 0644)
+	if err != nil {
+		log.Fatalf("Couldnt write db: %v", err)
+	}
+}
+
+func crudHandler(w http.ResponseWriter, r *http.Request) {
+	//id := r.URL.Query().Get("id")
+	switch r.Method {
+	case http.MethodGet:
+		// just get all
+	case http.MethodPut:
+		myVideos := []video{}
+		d := json.NewDecoder(r.Body)
+		err := d.Decode(&myVideos)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Printf("Couldnt decode: %v", err)
+			return
+		}
+		lock.Lock()
+		videos = myVideos
+		lock.Unlock()
+		writeVideos()
+		log.Printf("Updated: %+v", myVideos)
+	}
+	lock.RLock()
+	defer lock.RUnlock()
+	b, _ := json.Marshal(videos)
+	w.Write(b)
+	log.Printf("Returned: %+v", videos)
+}
+
+func ytDataHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		// no video id
+		w.WriteHeader(http.StatusBadRequest)
+		log.Printf("No video ID")
+		return
+	}
+	url := "https://www.youtube.com/oembed?url=http%3A//youtube.com/watch%3Fv%3D" + id
+	resp, err := http.Get(url)
+	if err != nil {
+		// could not connect
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Printf("Could not fetch metadata: %s", err)
+		return
+	}
+	w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+	l := resp.Header.Get("Content-Length")
+	if l != "" {
+		w.Header().Set("Content-Length", l)
+	}
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+	resp.Body.Close()
+	if resp.StatusCode == http.StatusOK {
+		log.Printf("Retrieved video metadata for: %+v", id)
+	} else {
+		log.Printf("Error retrieving video metadata for %+v: %v", id, resp.Status)
+	}
 }
--- src/app.js
+++ src/app.js
@@ -2,65 +2,142 @@
 
 let state = {
   items: [
-    { id: "X7hFERntlog", title: "Fearless Org" }, 
-    { id: "d_HHnEROy_w", title: "Stop managing" }, 
-    { id: "BCkCvay4-DQ", title: "Foos" }
+    { id: "X7hFERntlog", title: "Fearless Org", votes: 0 }, 
+    { id: "d_HHnEROy_w", title: "Stop managing", votes: -10 }, 
+    { id: "BCkCvay4-DQ", title: "Foos", votes: 1 }
   ],
+  selectedItem: "X7hFERntlog"
 };
-const items = () => {
-  return state.items.map( i => `
-    <li onclick="show('${i.id}')">
+const items = (id) => {
+  return state.items.sort((a, b) => b.votes - a.votes ).map( i => `
+    <li >
       <div>
-        <div>
-          ${i.title} 
+        <div onclick="setSelectedItem('${i.id}')">
+          ${i.title} (${i.votes} votes)
         </div>
         <div>
-          <img src="https://img.icons8.com/material/24/000000/circled-chevron-up.png" onclick="upvote(${i.id})" />
-          <img src="https://img.icons8.com/material/24/000000/circled-chevron-down.png" onclick="downvote(${i.id})" />
+          <img src="https://img.icons8.com/material/24/000000/circled-chevron-up.png" onclick="upvote('${i.id}')" />
+          <img src="https://img.icons8.com/material/24/000000/circled-chevron-down.png" onclick="downvote('${i.id}')" />
         </div>
       </div>
     </li>` ).join('');
 };
 
+const setSelectedItem = (i) => {
+  state.selectedItem = i;
+  show(i);
+};
+
 const show = (i) => {
+  if (!i) {
+    return;
+  }
   document.getElementById('player').setAttribute('src', 'https://www.youtube.com/embed/' + i);
   const item = state.items.find(item => item.id === i);
-  document.getElementById('title').innerHTML = item.title;
+  let title = 'preview';
+  if (item) {
+    title = item.title;
+  }
+  document.getElementById('title').innerHTML = title;
 };
 
 const upvote = (i) => {
+  const item = state.items.find(item => item.id === i);
+  item.votes++;
+  putVideos();
 }
 
 const downvote = (i) => {
+  const item = state.items.find(item => item.id === i);
+  item.votes--;
+  putVideos();
 }
 
+const getVideos = () => {
+  fetch(`/videos`, { 
+      method: 'get',
+      cache: "no-cache",
+    })
+  .then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    state.items = json;
+    //push({id: id, title: json.title});
+    reflop();
+  });
+}
+
+const cleanInput = (input) => {
+  const pref = "https://www.youtube.com/watch?v=";
+  if (input.startsWith(pref)) {
+    return input.substring(pref.length);
+  }
+  return input;
+}
+
+const putVideos = () => {
+  fetch(`/videos`, { 
+      method: 'PUT',
+      cache: "no-cache",
+      headers: {
+          "Content-Type": "application/json",
+      },
+      redirect: "follow", // manual, *follow, error
+      referrer: "no-referrer", // no-referrer, *client
+      body: JSON.stringify(state.items), // body data type must match "Content-Type" header
+    })
+  .then(function(response) {
+    console.log(response);
+    return response.json();
+  })
+  .then(function(json) {
+    console.log(json);
+    state.items = json;
+    reflop();
+  });
+};
+
+const preview = () => {
+  const id = cleanInput(document.getElementById('addbox').value);
+  if (id) {
+    show(id);
+  }
+};
+
 const add = () => {
-  const id = document.getElementById('addbox').value;
-  fetch(`/yt/data/${id}`, { 
+  const id = cleanInput(document.getElementById('addbox').value);
+  
+  fetch(`/yt/data?id=${id}`, { 
       method: 'get',
     })
   .then(function(response) {
-    var json = response.json();
-    state.items.push({id: id, title: json.title});
-    reflop();
+    console.log(response);
+    return response.json();
   })
-  .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
+  .then(function(json) {
+    console.log(JSON.stringify(json));
+    console.log(json.title);
+    let title = json.title;
+    if (title.length > 30) {
+      title = title.substring(0, 30) + " ..."; 
+    }
+    state.items.push({id: id, title: title});
+    state.selectedItem = id;
+    putVideos();
   });
 };
 
 // The router code. Takes a URL, checks against the list of supported routes and then renders the corresponding content page.
 const reflop = async () => {
-  const content = document.getElementById('page_content')
   const vidList = document.getElementById('videoList');
-  
-  if (content) {
-    content.innerHTML = '<h2>app</h2>';
-  }
   if (vidList) {
     vidList.innerHTML = items();
   }
+  show(state.selectedItem);
 }
 
 // Listen on page load:
-window.addEventListener('load', reflop);
+window.addEventListener('load', getVideos);
